cabaaf89ce0a4a236db0c3869f4ee8f1a4ac803e
Asvaron <Asvaron>
2021-09-06 17:56:21 +0300

    İnitial commit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+import turtle as t
 import random
-rafi = Turtle()
+rafi = t.Turtle()
 rafi.color("maroon")
 rafi.shape("triangle")
+t.colormode(255)
 
-color = ["maroon", "dark red", "brown", "firebrick", "medium purple", "light sky blue", "yellow", "midnight blue", "forest green"]
 
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    the_tuple = (r, g, b)
+    return the_tuple
+
+
+angle = ["0", "90", "180", "270"]
+rafi.width(15)
+rafi.speed(0)
 for shape in range (3,11):
-    rafi.color(random.choice(color))
+    rafi.color(random_color())
     for side in range(shape):
         rafi.forward(100)
         rafi.rt(360/shape)
+
+
+for _ in range(1000):
+    rafi.forward(30)
+    m_angle = int(random.choice(angle))
+    rafi.setheading(m_angle)
+    rafi.color(random_color())
 
 
 screen = Screen()
